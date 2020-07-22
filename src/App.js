3dbb90209fbbb7f6569693d13656a0eb21c8b119
Jakhongir Sabirov <jahon.080879@gmail.com>
2020-07-22 17:24:10 +0500

    Shop is done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import {Switch,Route} from 'react-router-dom'
-import ShopPage from './pages/shop/Shop.component';
+
 import HomePage from './pages/homepage/homepage.component';
-
-
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
-        <Switch>
-            <Route  exact path='/' component={HomePage}/>
-            <Route   path='/shop' component={ShopPage}/>
-        </Switch>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
