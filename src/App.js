519e17d83aaaf45e100263328e7755d5c397f8b0
Jakhongir Sabirov <jahon.080879@gmail.com>
2020-07-22 14:01:35 +0500

    How to send ...props into component and get it from other component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
-
+import {Switch,Route} from 'react-router-dom'
+import ShopPage from './pages/shop/Shop.component';
 import HomePage from './pages/homepage/homepage.component';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Switch>
+            <Route  exact path='/' component={HomePage}/>
+            <Route   path='/shop' component={ShopPage}/>
+
+        </Switch>
     </div>
   );
 }
