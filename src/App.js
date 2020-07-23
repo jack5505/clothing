984ec4e4637d8a93c16ecf7ea15738d12992aa8d
Jakhongir Sabirov <jahon.080879@gmail.com>
2020-07-23 16:44:51 +0500

    Write it down how to save into firebase but. I should see what heppened here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Sign from './pages/sign-in-sign-out/SignIn'
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component{
 
@@ -22,10 +22,30 @@ class App extends React.Component{
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-           this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-                this.setState({currentUser:user});
-            })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+
+                    console.log(this.state);
+                });
+            }
+
+            this.setState({ currentUser: userAuth });
+        });
     }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
 
 
     componentWillUnmount() {
