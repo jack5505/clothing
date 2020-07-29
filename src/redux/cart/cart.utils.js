241b3453ec52ec0d15af56c5b8e5768eb4b37ded
Jakhongir Sabirov <jahon.080879@gmail.com>
2020-07-29 17:15:00 +0500

    With error after this will be worked state

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,8 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
+
+
   if(existingCartItem){
       return cartItems.map(cartItem =>
           cartItemToAdd.id === cartItem.id ?
