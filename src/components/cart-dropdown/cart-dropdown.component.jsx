d79dc7c097de1426bd224613a31e97c92095dbb4
Jack <jahon.080879@gmail.com>
2020-08-01 22:23:22 +0500

    Lets start to build Checkout Page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,8 +7,9 @@ import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 import {createStructuredSelector} from "reselect";
 import {selectCartItems} from "../../redux/user/cart.selectors";
+import {toggleCartHidden} from "../../redux/cart/cart.actions";
 
-const CartDropdown = ({ cartItems,history }) => (
+const CartDropdown = ({ cartItems,history,dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -19,9 +20,14 @@ const CartDropdown = ({ cartItems,history }) => (
               (<span className='empty-message'>You cart is empty</span>)
     }
     </div>
-    <CustomButton onClick={()=> history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={()=> {
+        history.push('/checkout');
+        dispatch(toggleCartHidden())
+    }}>GO TO CHECKOUT</CustomButton>
   </div>
 );
+
+
 
 const mapStateToProps = createStructuredSelector({
   cartItems:selectCartItems
