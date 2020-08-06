84e26eaecb2151fc76636ad0581a297739f9c692
Sabirov Jakhongir <jahon.080879@gmail.com>
2020-08-06 15:41:40 +0500

    Directory State into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,19 +2,20 @@ import { combineReducers } from 'redux';
 import {persistReducer} from "redux-persist";
 import storage from 'redux-persist/lib/storage'
 
-
+import directoryReducer from './directory/directory.reducer'
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
 const perSistConfig = {
   key:'root',
   storage,
-  whitelist:['cart','user']
+  whitelist:['cart']
 };
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory:directoryReducer
 });
 
 
