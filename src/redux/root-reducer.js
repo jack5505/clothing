33f1e5aba1a4d8bad40ff57bf848b69bcae81e05
Sabirov Jakhongir <jahon.080879@gmail.com>
2020-08-07 17:24:17 +0500

    Collection state into project

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,7 +5,7 @@ import storage from 'redux-persist/lib/storage'
 import directoryReducer from './directory/directory.reducer'
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-
+import shopReducer from './shop/shop.reducer'
 const perSistConfig = {
   key:'root',
   storage,
@@ -15,7 +15,8 @@ const perSistConfig = {
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-    directory:directoryReducer
+    directory:directoryReducer,
+    shop:shopReducer
 });
 
 
