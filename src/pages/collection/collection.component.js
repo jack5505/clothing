486a469e3faeced6fe41918347ddca5d47de77a1
Jack <jahon.080879@gmail.com>
2020-08-09 18:07:22 +0500

    Learn Data Normalization

--- src/pages/collection/collection.component.js
+++ src/pages/collection/collection.component.js
@@ -6,10 +6,16 @@ import './collection.style.scss'
 
 
 const collectionPage = ({collection}) => {
-    console.log(collection);
+
+    const {title,items} = collection;
     return(
-    <div className="category">
-        <h2>CATEGORY PAGE</h2>
+    <div className="collection-page">
+        <h2 className='title'>{ title }</h2>
+        <div className='items'>
+            {
+                items.map(item => <CollectionItem key={item.id} item={item}/>)
+            }
+        </div>
     </div>)
 };
 
