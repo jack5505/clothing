bbd6a574777c567b6d224c4ca243b9f78438c5ed
Jack <jahon.080879@gmail.com>
2020-08-09 18:17:11 +0500

    Almost is ready

--- src/components/collections-overview/collections.overview.component.js
+++ src/components/collections-overview/collections.overview.component.js
@@ -5,7 +5,7 @@ import {connect} from 'react-redux'
 import './collections.overview.style.scss'
 import {createStructuredSelector} from 'reselect'
 import CollectionPreview from "../collection-preview/collection-preview";
-import {selectCollections} from "../../redux/shop/shop.selector";
+import {selectCollectionOverview} from "../../redux/shop/shop.selector";
 
 const CollectionsOverview = ({collections}) => (
  <div className='collections-overview'>
@@ -16,7 +16,7 @@ const CollectionsOverview = ({collections}) => (
 );
 
 const  mapStateToProps  = createStructuredSelector({
-    collections:selectCollections
-})
+    collections:selectCollectionOverview
+});
 
 export default connect(mapStateToProps)(CollectionsOverview)
