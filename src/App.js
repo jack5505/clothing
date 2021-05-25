53ac2f527088f8f0ccf6bfdd0d9f7a74d6fab037
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-05-25 15:00:16 +0500

    Learn Define of function inside of constructor

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ import {CardList} from "./components/card-list/cardComponents";
 import {SearchBox} from "./components/search-box/SearchBoxComponent";
 
 class  App  extends Component{
-
+    /**
+     * Our app component and the method in which we can do it is to define it in our constructor because our
+     constructor is the code that runs first before anything gets called.
+     */
   constructor(){
       super();
       this.state = {
         monsters:[],
         searchField:''
       }
+
+      /**
+       * The reason for this is because JavaScript by default, doesn't set its scope of this on functions.
+       * @type {any}
+       */
+      this.handleChange = this.handleChange.bind(this)
   }
 
 
     componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(users=> this.setState({monsters:users}))
+            fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users=> this.setState({monsters:users}))
+    }
 
-  }
+    handleChange(e){
+        this.setState({searchField: e.target.value})
+    }
 
     render() {
         const {monsters,searchField} = this.state;
@@ -28,7 +40,7 @@ class  App  extends Component{
         // const email = monsters.filter(monster => monster.email.toLowerCase().includes(searchField.toLowerCase()));
         return (
             <div className="App">
-                <SearchBox placeholder="search monsters" handleChange={e => this.setState({searchField: e.target.value})}/>
+                <SearchBox placeholder="search monsters" handleChange={this.handleChange}/>
                 <CardList monsters={filteredMonsters}/>
             </div>
         );
