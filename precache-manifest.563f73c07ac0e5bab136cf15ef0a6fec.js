35258761dca9089737a6729e4222b2fe9593b8fe
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-05-26 10:47:41 +0500

    Updates

---
+++ precache-manifest.563f73c07ac0e5bab136cf15ef0a6fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696e006bcd91a9f95d4c17fef7643009",
+    "url": "/clothing/index.html"
+  },
+  {
+    "revision": "10d42e5d21d7c93c5451",
+    "url": "/clothing/static/css/main.5b435409.chunk.css"
+  },
+  {
+    "revision": "3117ce46c6d490cc7056",
+    "url": "/clothing/static/js/2.9ab24f1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clothing/static/js/2.9ab24f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d42e5d21d7c93c5451",
+    "url": "/clothing/static/js/main.2f0613a5.chunk.js"
+  },
+  {
+    "revision": "bb6c0e494451c7ecc836",
+    "url": "/clothing/static/js/runtime-main.43df5736.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clothing/static/media/logo.5d5d9eef.svg"
+  }
+]);
